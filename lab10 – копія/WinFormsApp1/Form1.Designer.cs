f69da84d283f61e2f34d5743747385e4fa14b71a
anastasiialoskutova <anastasiia.loskutova@lnu.edu.ua>
2025-11-13 17:28:14 +0200

    Додаткове завдання

---
+++ lab10 – копія/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            dataGridView2 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Оберіть розмірність СНР";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 143);
+            label2.Name = "label2";
+            label2.Size = new Size(334, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Введіть елементи вектора початкових умов Х0";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(258, 62);
+            numericUpDown1.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(41, 27);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(64, 180);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(150, 188);
+            dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(124, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Розв'язати";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(280, 409);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Очистити";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(450, 409);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 6;
+            button3.Text = "Завершити";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(429, 143);
+            label3.Name = "label3";
+            label3.Size = new Size(201, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Введіть необхідні значення";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(418, 185);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Точність Eps";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(467, 251);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 20);
+            label5.TabIndex = 9;
+            label5.Text = "KMax";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(720, 143);
+            label6.Name = "label6";
+            label6.Size = new Size(177, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Вектор розв'язку СНР X*";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(700, 418);
+            label7.Name = "label7";
+            label7.Size = new Size(48, 20);
+            label7.TabIndex = 11;
+            label7.Text = "N_Iter";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(519, 182);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 12;
+            textBox1.Text = "1e-6";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(519, 248);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(758, 415);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 14;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(700, 180);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersVisible = false;
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.Size = new Size(216, 188);
+            dataGridView2.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(950, 480);
+            Controls.Add(dataGridView2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "ЛАА Метод Ньютона для СНР";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private DataGridView dataGridView2;
+    }
+}
---
+++ lab10 – копія/WinFormsApp1/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // 1. Оголошення полів класу (використовуємо індексацію 1..N)
+        private int N = 3;                                  // Розмірність СНР
+        private double[,] Ja = new double[4, 4];            // Матриця Якобі
+        private double[] X0 = new double[4];                // Вектор початкових умов
+        private double[] X = new double[4];                 // Вектор розв'язку СНР (поточне наближення)
+        private double[] F = new double[4];                 // Вектор лівої частини СНР
+        private double[] Fp = new double[4];                // Робочий вектор для обчислення Якобі
+        private double[] Dx = new double[4];                // Вектор нев'язки (розв'язок СЛАР)
+
+        // Поля для LU-розкладу
+        private double[,] JaLU = new double[4, 4];          // Матриця для зберігання LU-факторів J(X0)
+        private int[] P = new int[4];                       // Вектор перестановок
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Налаштування полів форми при запуску
+            N = (int)numericUpDown1.Value;
+            ResizeArrays(N);
+            SetupDataGridViews();
+
+            // Прив'язка обробників подій
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;
+        }
+
+        // --- Допоміжні методи для інтерфейсу та ініціалізації ---
+        private void ResizeArrays(int size)
+        {
+            N = size;
+            int arraySize = N + 1 > 2 ? N + 1 : 2;
+
+            Ja = new double[arraySize, arraySize];
+            X0 = new double[arraySize];
+            X = new double[arraySize];
+            F = new double[arraySize];
+            Fp = new double[arraySize];
+            Dx = new double[arraySize];
+
+            // Ініціалізація полів LU-розкладу
+            JaLU = new double[arraySize, arraySize];
+            P = new int[arraySize];
+        }
+
+        private void SetupDataGridViews()
+        {
+            // Налаштування DataGridView1 (для X0)
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.Columns[0].Width = 70;
+
+            // Налаштування DataGridView2 (для X*)
+            dataGridView2.ColumnCount = 1;
+            dataGridView2.RowHeadersVisible = false;
+            dataGridView2.ColumnHeadersVisible = false;
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.ReadOnly = true;
+            dataGridView2.Columns[0].Width = 150;
+
+            ResizeDataGridViews(N);
+        }
+
+        private void ResizeDataGridViews(int size)
+        {
+            dataGridView1.RowCount = size;
+            dataGridView2.RowCount = size;
+        }
+
+        // --- Обробник зміни розмірності ---
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int newN = (int)numericUpDown1.Value;
+            if (newN >= 1)
+            {
+                ResizeArrays(newN);
+                ResizeDataGridViews(newN);
+            }
+        }
+
+
+        // --- Методи класу, що реалізують алгоритм ---
+
+        /// <summary>
+        /// Метод для обчислення значень вектора F лівої частини СНР F(X)=0.
+        /// </summary>
+        public void FM(double[] X, ref double[] f)
+        {
+            if (N == 3)
+            {
+                // f₁(x₁, x₂, x₃) = x₁ + e^(x₁-1) + (x₂ + x₃)² - 27
+                f[1] = X[1] + Math.Exp(X[1] - 1.0D) + (X[2] + X[3]) * (X[2] + X[3]) - 27.0D;
+
+                // f₂(x₁, x₂, x₃) = x₁ * e^(x₂-2) + x₃² - 10
+                f[2] = X[1] * Math.Exp(X[2] - 2.0D) + X[3] * X[3] - 10.0D;
+
+                // f₃(x₁, x₂, x₃) = x₃ + Sin(x₂-2) + x₂² - 7
+                f[3] = X[3] + Math.Sin(X[2] - 2.0D) + X[2] * X[2] - 7.0D;
+            }
+            else if (N == 1)
+            {
+                // Для N=1 (одне нелінійне рівняння)
+                f[1] = X[1] * X[1] - 4.0D;
+            }
+        }
+
+        /// <summary>
+        /// Метод для чисельного обчислення значень матриці Якобі Ja.
+        /// </summary>
+        public double[,] Jacob(double[] X)
+        {
+            const double Q = 0.000001D;
+
+            double[] X_copy = (double[])X.Clone();
+
+            FM(X_copy, ref F);
+
+            for (int j = 1; j <= N; j++)
+            {
+                X_copy[j] = X_copy[j] + Q;
+                FM(X_copy, ref Fp);
+
+                for (int i = 1; i <= N; i++)
+                {
+                    Ja[i, j] = (Fp[i] - F[i]) / Q;
+                }
+
+                X_copy[j] = X_copy[j] - Q;
+            }
+
+            return Ja;
+        }
+
+        // --- Обробники подій кнопок ---
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+
+            for (int i = 0; i < N; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = "";
+                dataGridView2.Rows[i].Cells[0].Value = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (N < 1)
+                {
+                    MessageBox.Show("Розмірність СНР має бути мінімум 1.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // 1. Зчитування параметрів та ініціалізація
+                double Eps = Convert.ToDouble(textBox1.Text); // Точність
+                int KMax = Convert.ToInt32(textBox2.Text);    // Макс. кількість ітерацій
+
+                // Зчитування вектора початкових умов X0 та ініціалізація X
+                for (int i = 1; i <= N; i++)
+                {
+                    if (dataGridView1.Rows[i - 1].Cells[0].Value == null || string.IsNullOrWhiteSpace(dataGridView1.Rows[i - 1].Cells[0].Value.ToString()))
+                    {
+                        throw new FormatException($"Не введено значення для X0[{i}]");
+                    }
+
+                    X0[i] = Convert.ToDouble(dataGridView1.Rows[i - 1].Cells[0].Value);
+                    X[i] = X0[i];
+                    dataGridView2.Rows[i - 1].Cells[0].Value = "";
+                }
+
+                // --- 2. ЕТАП ПЕРЕД ЦИКЛОМ (Обчислення Якобі та LU-розклад) ---
+
+                // 2.1. Обчислення матриці Якобі J(X0)
+                Ja = Jacob(X0);
+
+                // 2.2. Копіюємо J(X0) для LU-розкладу (JaLU міститиме L і U)
+                JaLU = (double[,])Ja.Clone();
+
+                // 2.3. LU-розклад матриці Якобі J(X0)
+                LUSolver.Decompose(JaLU, N, out P);
+
+                // --- 3. ЦИКЛ ІТЕРАЦІЙ ---
+                double dxmax = double.MaxValue;
+                int k;
+
+                for (k = 1; k <= KMax; k++)
+                {
+                    // 3.1. Обчислення F(X(k))
+                    FM(X, ref F);
+
+                    // 3.2. Розв'язання СЛАР (JaLU) * Dx = F (тільки прямий/зворотний хід)
+                    // F - права частина (нев'язка), Dx - розв'язок СЛАР (поправка)
+                    LUSolver.Solve(JaLU, F, ref Dx, P, N);
+
+                    // 3.3. Ітерація Ньютона та обчислення норми
+                    dxmax = 0.0D;
+                    for (int i = 1; i <= N; i++)
+                    {
+                        // Ітерація Ньютона: X(k+1) = X(k) - Dx(k)
+                        X[i] = X[i] - Dx[i];
+
+                        // Норма: Dxmax = max(|Dx[i]|)
+                        if (Math.Abs(Dx[i]) > dxmax)
+                            dxmax = Math.Abs(Dx[i]);
+                    }
+
+                    // 3.4. Перевірка умови закінчення
+                    if (dxmax < Eps)
+                    {
+                        textBox3.Text = Convert.ToString(k);
+                        for (int i = 1; i <= N; i++)
+                        {
+                            dataGridView2.Rows[i - 1].Cells[0].Value = X[i].ToString("F15");
+                        }
+
+                        MessageBox.Show("Розв'язок СНР знайдено");
+                        return;
+                    }
+                }
+
+                // 4. Якщо КМах ітерацій не досягнуто
+                textBox3.Text = Convert.ToString(KMax);
+                MessageBox.Show($"Неможливо розв'язати: Збіжності не досягнуто за максимальну кількість ітерацій ({KMax})", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Помилка вхідних даних: Перевірте формат чисел (наприклад, 0.0000001, 15) для Eps/KMax та X0.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show($"Помилка обчислення: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Виникла невідома помилка: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    // Клас для реалізації LU-розкладу (замість Гауса)
+    public static class LUSolver
+    {
+        /// <summary>
+        /// LU-розклад матриці A (з частковим вибором)
+        /// Зберігає L та U фактори в одній матриці LU.
+        /// </summary>
+        public static void Decompose(double[,] LU, int size, out int[] p)
+        {
+            // Ініціалізація вектора перестановок P (1-based)
+            p = new int[size + 1];
+            for (int i = 1; i <= size; i++)
+                p[i] = i;
+
+            for (int k = 1; k <= size; k++)
+            {
+                // 1. Частковий вибір головного елемента (Pivot)
+                int maxRow = k;
+                double maxVal = 0;
+                for (int i = k; i <= size; i++)
+                {
+                    if (Math.Abs(LU[i, k]) > maxVal)
+                    {
+                        maxVal = Math.Abs(LU[i, k]);
+                        maxRow = i;
+                    }
+                }
+
+                if (maxVal == 0)
+                    throw new InvalidOperationException("Матриця Якобі вироджена.");
+
+                // 2. Зміна рядків у матриці LU та векторі P
+                if (maxRow != k)
+                {
+                    // Swap P vector elements
+                    int tempP = p[k];
+                    p[k] = p[maxRow];
+                    p[maxRow] = tempP;
+
+                    // Swap rows in LU matrix
+                    for (int j = 1; j <= size; j++)
+                    {
+                        double temp = LU[k, j];
+                        LU[k, j] = LU[maxRow, j];
+                        LU[maxRow, j] = temp;
+                    }
+                }
+
+                // 3. LU-факторизація (Doolittle)
+                for (int i = k + 1; i <= size; i++)
+                {
+                    // L[i, k] - множник
+                    LU[i, k] = LU[i, k] / LU[k, k];
+
+                    // Оновлення підматриці (формування U)
+                    for (int j = k + 1; j <= size; j++)
+                    {
+                        LU[i, j] = LU[i, j] - LU[i, k] * LU[k, j];
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Розв'язує СЛАР L*U*x = P*B (прямий та зворотний хід).
+        /// </summary>
+        public static void Solve(double[,] LU, double[] B, ref double[] x, int[] p, int size)
+        {
+            // Вектор Y для проміжного розв'язку L*Y = P*B
+            double[] Y = new double[size + 1];
+
+            // 1. Прямий хід (Forward Substitution): L*Y = P*B
+            for (int i = 1; i <= size; i++)
+            {
+                // Y[i] = P[B[i]] - Sum(L[i,k] * Y[k])
+
+                // Permutation P*B
+                Y[i] = B[p[i]];
+
+                // Subtraction of the sum
+                for (int k = 1; k < i; k++)
+                {
+                    // L-фактор LU[i, k] знаходиться під діагоналлю
+                    Y[i] -= LU[i, k] * Y[k];
+                }
+            }
+
+            // 2. Зворотний хід (Backward Substitution): U*X = Y
+            for (int i = size; i >= 1; i--)
+            {
+                double sum = 0.0;
+                // Sum(U[i,k]*X[k])
+                for (int k = i + 1; k <= size; k++)
+                {
+                    // U-фактор LU[i, k] знаходиться на діагоналі та над нею
+                    sum += LU[i, k] * x[k];
+                }
+                // X[i] = (Y[i] - Sum) / U[i,i]
+                x[i] = (Y[i] - sum) / LU[i, i];
+            }
+        }
+    }
+}
